f762f91499bb2aa284cbb33f35bac2c634a89b04
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-07-22 12:43:19 -0400

    Add temporary home page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
