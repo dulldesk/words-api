4334196d444466bf1ccb6f30a9854719be87432a
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-07-23 11:09:24 -0400

    Move lines around
    For organizational purposes

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
-
-const indexRouter = require('./routes/index');
 
 const app = express();
 app.use(cors());
 
+const indexRouter = require('./routes/index');
 const randomRouter = require("./routes/random");
+
 app.use('/', indexRouter);
 
 app.get("/random/:type", randomRouter.genType);
-
 app.get("/random/:type/:letter", randomRouter.genLetterOfType);
 
 
