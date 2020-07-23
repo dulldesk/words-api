e5164639ea2dc9ab1d1a320f1392581c4a938f6e
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-07-23 11:12:53 -0400

    Catch status errors

--- app.js
+++ app.js
@@ -12,5 +12,12 @@ app.use('/', indexRouter);
 app.get("/random/:type", randomRouter.genType);
 app.get("/random/:type/:letter", randomRouter.genLetterOfType);
 
+app.use(function (req, res, next) {
+	res.status(404).send("404 not found");
+});
+
+app.use(function (err, req, res, next) {
+	if (err) res.status(500).send('500 server error');
+});
 
 module.exports = app;
